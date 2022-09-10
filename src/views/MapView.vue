18d56e5f89316d7b4b15da4a0f64a78df98e9dc5
stelaukin <stelaukin@outlook.com>
2022-09-10 12:17:52 +1000

    working the map out yeet

---
+++ src/views/MapView.vue
@@ -0,0 +1,54 @@
+<!-- MAPS API KEY -->
+<!-- AIzaSyAuDdI55xkd_BCgEmjCz-xkFzrPOIqBjHQ --> 
+
+
+<template>
+  <div>
+    <GoogleMap style="width: 100%; height: 100vh" :center="center" :zoom="7">
+      <!-- <Marker :options="{ position: center }" /> -->
+      <MarkerCluster>
+        <Marker
+          v-for="pos in markerPos"
+          v-bind:key="pos.id"
+          v-bind:options="pos"
+        >
+        <InfoWindow>
+            {{pos.title}}
+        </InfoWindow>
+        </Marker>
+      </MarkerCluster>
+    </GoogleMap>
+    <ul>
+      <li v-for="toilet in toilets" v-bind:key="toilet.FacilityID">
+        {{ toilet.Name }}
+      </li>
+    </ul>
+  </div>
+</template>
+  
+  <script>
+import { defineComponent } from "vue";
+import { GoogleMap, Marker, MarkerCluster, InfoWindow } from "vue3-google-map";
+//   import all toilt data from file
+import rawToilets from "../assets/data/toiletsTas.json";
+export default defineComponent({
+  data() {
+    return {
+      toilets: rawToilets,
+    };
+  },
+  components: { GoogleMap, Marker, MarkerCluster, InfoWindow },
+  setup() {
+    // turns raw data into google map location data using vue3-google-map package
+    const markerPos = rawToilets.map((t) => ({
+      position: { lat: Number(t.Latitude), lng: Number(t.Longitude) },
+      label: t.Name.charAt(0),
+      id: t.FacilityID,
+      title: t.Name,
+    }));
+
+    const center = { lat: -41.93, lng: 147.49 };
+    return { center, markerPos };
+  },
+});
+</script>
